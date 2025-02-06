c3ec38425fc7c3536976d869c88eb977d50ebcb1
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-02-06 16:35:12 +0800

    Add files via upload

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -8,12 +8,6 @@ return {
         dependencies = {
             "williamboman/mason-lspconfig.nvim",
             "neovim/nvim-lspconfig",
-	    "williamboman/mason-lspconfig.nvim",
-	    "mfussenegger/nvim-dap",
-	    "rcarriga/nvim-dap-ui",
-	    "nvim-neotest/nvim-nio",
-	    "mhartington/formatter.nvim",
-	    "mfussenegger/nvim-lint",
         },
     },
 }
