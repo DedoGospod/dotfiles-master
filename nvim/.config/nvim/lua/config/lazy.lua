79752034a95e05aa196102b2375f8b0724f16477
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-02-06 22:22:29 +0800

    Update lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,3 +1,17 @@
+-- [SETTINGS]
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = false
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -15,11 +29,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -28,13 +37,32 @@ require("lazy").setup({
     { import = "plugins" },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
+
+
+-- [KEYMAPPINGS]
+
+-- Telescope 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
+
+-- [CONFIGURATIONS]
+
+
+-- NVIM TREE 
+vim.keymap.set('n', '<C-n>', function()
+  require('nvim-tree.api').tree.toggle()
+end, { desc = 'Toggle file tree' })
+
+
+-- TELESCOPE
 require('telescope').setup {
   defaults = {
     vimgrep_arguments = {
@@ -55,34 +83,69 @@ require('telescope').setup {
   },
 }
 
-require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
+-- LSP Setup with nvim-lspconfig
+local lspconfig = require('lspconfig')
+
+-- Example: Python (pyright)
+lspconfig.basedpyright.setup {}
+
+-- Example: JavaScript and TypeScript (tsserver)
+--lspconfig.tsserver.setup {} (This isnt maintained)
+
+-- Example: Go (gopls)
+lspconfig.gopls.setup {}
+
+-- Example: Rust (rust_analyzer)
+lspconfig.rust_analyzer.setup {}
+
+-- Example: Lua (sumneko_lua)
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },  -- to let Lua know that `vim` is a global variable
+      },
     },
   },
-  -- Custom install root directory (optional)
-  install_root_dir = vim.fn.stdpath("data") .. "/mason",
+}
+
+-- Automatically install missing servers with mason
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "basedpyright", "rust_analyzer", }, -- Add more as needed
 })
 
--- Turn on line numbers
-vim.wo.number = true
+-- Completion setup (nvim-cmp)
+local cmp = require('cmp')
 
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'luasnip' },
+  },
+})
 
+-- Trouble plugin for LSP diagnostics
+require("trouble").setup()
 
---KEYMAPPINGS
-
-
--- Telescope 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
---Nvim Tree 
-vim.keymap.set('n', '<C-n>', function()
-  require('nvim-tree.api').tree.toggle() 
-end, { desc = 'Toggle file tree' })
+-- Lualine setup for status line
+require("lualine").setup {
+  options = {
+    theme = 'catppuccin',
+  },
+}
