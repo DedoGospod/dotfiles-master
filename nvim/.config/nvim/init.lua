2b254447799b5661627fa9426a2a801e9913f3de
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-02-06 22:21:31 +0800

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,11 @@
-
---Lazy pkg manager
+--LOAD LAZY
 require("config.lazy")
 
 --Catppuccin theme
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
 
 --Allow hyprlang to recognise hyprland files 
 vim.filetype.add({
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 })
+
