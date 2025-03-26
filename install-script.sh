8afc7c6cddba056a4e3bfd897e6c5881185f0343
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-03-26 17:26:29 +0800

    Create install-script.sh

---
+++ install-script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Update the system and install pacman packages
+echo "Updating system..."
+sudo pacman -Syu --noconfirm || exit 1
+
+# List of pacman packages
+pacman_packages=(
+  hyprland
+  kitty
+  hypridle
+  hyprlock
+  hyprpaper
+  neovim
+  starship
+  waybar
+  wofi
+  yazi
+  zsh
+  nautilus
+  swaync
+  sway-audio-idle-inhibit
+  xdg-desktop-portal-gtk
+  xdg-desktop-portal-hyprland
+  hyprpolkitagent
+  wlsunset
+  zoxide
+  zsh-syntax-highlighting
+  fzf
+  qt6ct
+  adwaita-dark
+  btop
+  dbus
+)
+
+echo "Installing pacman packages..."
+sudo pacman -S --noconfirm "${pacman_packages[@]}" || exit 1
+
+# List of AUR packages
+aur_packages=(
+  trash-cli
+)
+
+echo "Installing AUR packages..."
+yay -S --noconfirm "${aur_packages[@]}" || exit 1
+
+# Set zsh as the default shell
+echo "Setting zsh as the default shell..."
+chsh -s $(which zsh) || exit 1
+
+echo "Installation and setup complete!"
