22969c128381e0e8884771538875606f8533f2aa
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-04-02 15:08:53 +0800

    Update lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -90,11 +90,10 @@ require("mason-lspconfig").setup({
 local cmp = require('cmp')
 cmp.setup({
   snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
+mapping = cmp.mapping.preset.insert({
+    ['<Down>'] = cmp.mapping.select_next_item(),
+    ['<Up>'] = cmp.mapping.select_prev_item(),
+    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
   }),
   sources = {
     { name = 'nvim_lsp' },
