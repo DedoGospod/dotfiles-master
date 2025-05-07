b77d4c57bf57d08a6359b942fe70f1c07a5a249c
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-07 16:09:58 +0800

    Update lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,10 +1,3 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", "https://github.com/folke/lazy.nvim.git", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- Plugin setup with lazy.nvim
 require("lazy").setup({
   spec = {
