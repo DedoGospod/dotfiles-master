d2265c6a3c3cbda6c337084d9b0ef5233c691bfc
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-07 16:03:25 +0800

    Update compiler-nvim.lua

--- nvim/.config/nvim/lua/plugins/compiler-nvim.lua
+++ nvim/.config/nvim/lua/plugins/compiler-nvim.lua
@@ -5,9 +5,10 @@ return {
     dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
     opts = {},
   },
-  { -- The task runner we use
+  { -- overseer.nvim (now lazy-loads on its own commands)
     "stevearc/overseer.nvim",
     commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+    cmd = { "OverseerOpen", "OverseerToggle", "OverseerRun" }, -- Overseer's own commands
     opts = {
       task_list = {
         direction = "bottom",
