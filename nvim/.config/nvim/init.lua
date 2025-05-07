9d082daeb892d6888fb327dc23f0bf2ddfaad280
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-07 15:27:23 +0800

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,42 @@
---LOAD LAZY
+-- Basic settings
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.showmode = false
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Sync clipboard between OS and Neovim 
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Load plugin manager
 require("config.lazy")
 
---Catppuccin theme
+-- Disable virtual_text since it's redundant due to lsp_lines
+vim.diagnostic.config({
+    virtual_text = false,  -- Disable inline text
+    virtual_lines = true,  -- Enable virtual lines below code for diagnostics
+})
+
+-- Set colorscheme
 vim.cmd.colorscheme("catppuccin")
 
---Allow hyprlang to recognise hyprland files 
+-- Filetype detection for Hyprland
 vim.filetype.add({
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 })
