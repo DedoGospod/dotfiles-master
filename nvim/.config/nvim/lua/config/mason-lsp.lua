9d8a0a8827d920f870231bbd76c56687d28ab7ed
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-13 05:43:02 +0800

    Update mason-lsp.lua

--- nvim/.config/nvim/lua/config/mason-lsp.lua
+++ nvim/.config/nvim/lua/config/mason-lsp.lua
@@ -15,8 +15,115 @@ require("mason-lspconfig").setup({
           settings = {
             Lua = {
               diagnostics = { globals = { 'vim' } },
+              workspace = { checkThirdParty = false }, -- Don't check libraries you didn't write
+              telemetry = { enable = false }, -- Opt-out of telemetry
             },
           },
+        }
+      elseif server_name == "basedpyright" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic", -- Or "strict" for more rigorous checking
+                diagnosticMode = "openFilesOnly", -- Or "workspace" to check all project files
+                useLibraryCodeForTypes = true, -- Improve type inference
+              },
+            },
+          },
+        }
+      elseif server_name == "rust_analyzer" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = true,
+              cargo = {
+                allFeatures = true, -- Enable all features by default
+              },
+              procMacro = {
+                enable = true, -- Enable procedural macro support
+              },
+            },
+          },
+        }
+      elseif server_name == "gopls" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+                unreachable = true,
+                nilness = true,
+                shadow = true,
+                unusedwrite = true,
+              },
+              staticcheck = true, -- Enable staticcheck
+              usePlaceholders = true, -- Add placeholders for function arguments
+            },
+          },
+        }
+      elseif server_name == "bashls" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          settings = {
+            bashIde = {
+              globExcludePatterns = { "**/node_modules/**" }, -- Example exclusion
+            },
+          },
+        }
+      elseif server_name == "html" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          settings = {
+            html = {
+              format = {
+                enable = true,
+                indentInnerHtml = true,
+                wrapLineLength = 120,
+              },
+            },
+          },
+        }
+      elseif server_name == "zls" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          -- Zig doesn't have many common LSP settings yet, but i might add formatter config here later
+        }
+      elseif server_name == "ts_ls" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          settings = {
+            typescript = {
+              tsdk = {
+                enable = true,
+                autoSearchSystemTs = true,
+              },
+            },
+            javascript = {
+              tsdk = {
+                enable = true,
+                autoSearchSystemTs = true,
+              },
+            },
+            format = {
+              enable = true,
+              semicolons = "always",
+              singleQuote = true,
+            },
+          },
+        }
+      elseif server_name == "clangd" then
+        lspconfig[server_name].setup {
+          capabilities = capabilities,
+          -- You might want to configure compileCommands.json path or other clangd flags here
+          -- settings = {
+          --   ["clangd"] = {
+          --     arguments = { "--compile-commands-dir=build" },
+          --   },
+          -- },
         }
       else
         lspconfig[server_name].setup {
