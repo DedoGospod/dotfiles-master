300c881e37efd0e5e620c5b494de82273fffb792
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-14 04:28:28 +0800

    Update telescope.lua

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,4 +2,6 @@ return {
     'nvim-telescope/telescope.nvim',
       dependencies = { 'nvim-lua/plenary.nvim' },
 	             { 'nvim-telescope/telescope-fzf-native.nvim' },
+	cmd = 'Telescope',
+	lazy = true,
 }
