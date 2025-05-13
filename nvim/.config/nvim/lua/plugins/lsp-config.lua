7ea76113685a9bf2794300fa6025191f9fff7aea
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-14 03:34:17 +0800

    Update lsp-config.lua

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,48 +1,21 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            -- === nvim-lint setup ===
-            local lint = require("lint")
-            lint.linters_by_ft = {
-                sh = { "shellcheck" },
-                bashrc = { "shellcheck" },
-                env = { "shellcheck" },
-            }
-
-            -- Auto-run linting on save and file open
-            vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
-                callback = function()
-                    lint.try_lint()
-                end,
-            })
-        end,
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-vsnip",
-            "hrsh7th/vim-vsnip",
-            "mfussenegger/nvim-dap",
-            "rcarriga/nvim-dap-ui",
-            "nvim-neotest/nvim-nio",
-            "mfussenegger/nvim-lint",
-            "mhartington/formatter.nvim",
-            "windwp/nvim-autopairs",
-            "L3MON4D3/LuaSnip",
-            "rafamadriz/friendly-snippets",
-        },
-    },
-
-    -- lsp_lines.nvim
-    {
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        config = function()
-            require("lsp_lines").setup()
-        end,
-    },
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-vsnip",
+  "hrsh7th/vim-vsnip",
+  "mfussenegger/nvim-dap",
+  "rcarriga/nvim-dap-ui",
+  "nvim-neotest/nvim-nio",
+  "mfussenegger/nvim-lint",
+  "windwp/nvim-autopairs",
+  "L3MON4D3/LuaSnip",
+  "rafamadriz/friendly-snippets",
+  'stevearc/conform.nvim',
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 }
