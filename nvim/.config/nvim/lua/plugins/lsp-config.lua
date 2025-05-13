91f5af2aa5407abc1c005dfd6b5d6ab852a48680
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-14 03:39:55 +0800

    Update lsp-config.lua

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,21 +1,45 @@
 return {
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-vsnip",
-  "hrsh7th/vim-vsnip",
-  "mfussenegger/nvim-dap",
-  "rcarriga/nvim-dap-ui",
-  "nvim-neotest/nvim-nio",
-  "mfussenegger/nvim-lint",
-  "windwp/nvim-autopairs",
-  "L3MON4D3/LuaSnip",
-  "rafamadriz/friendly-snippets",
-  'stevearc/conform.nvim',
-  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  -- Mason (must come before mason-lspconfig)
+  { "williamboman/mason.nvim" },
+
+  -- mason-lspconfig (depends on mason.nvim)
+  { "williamboman/mason-lspconfig.nvim", dependencies = { "williamboman/mason.nvim" } },
+
+  -- LSP Configuration (depends on mason and mason-lspconfig)
+  { "neovim/nvim-lspconfig" },
+
+  -- Completion (order matters for cmp)
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-buffer", dependencies = { "hrsh7th/nvim-cmp" } },
+  { "hrsh7th/cmp-path", dependencies = { "hrsh7th/nvim-cmp" } },
+  { "hrsh7th/cmp-cmdline", dependencies = { "hrsh7th/nvim-cmp" } },
+  { "hrsh7th/cmp-vsnip", dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/vim-vsnip" } },
+  { "hrsh7th/cmp-nvim-lsp", dependencies = { "hrsh7th/nvim-cmp", "neovim/nvim-lspconfig" } },
+
+  -- Snippets
+  { "hrsh7th/vim-vsnip" },
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip", dependencies = { "hrsh7th/nvim-cmp", "L3MON4D3/LuaSnip" } },
+  { "rafamadriz/friendly-snippets", dependencies = { "L3MON4D3/LuaSnip" } },
+
+  -- Debugging
+  { "mfussenegger/nvim-dap" },
+  { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
+
+  -- Testing
+  { "nvim-neotest/nvim-nio" }, -- Assuming this is the core neotest plugin
+
+  -- Linting
+  { "mfussenegger/nvim-lint" },
+
+  -- Auto Pairs
+  { "windwp/nvim-autopairs" },
+
+  -- Formatting
+  { 'stevearc/conform.nvim' },
+
+  -- Utility/Other
+  { "https://git.sr.ht/~whynothugo/lsp_lines.nvim", dependencies = { "neovim/nvim-lspconfig" } },
+
+  -- Add any other plugins you have below
 }
