869a0b0e47b0337893b737062cce53a0d64af155
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-17 13:04:46 +0800

    Update lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,10 +1,10 @@
 -- Plugin setup with lazy.nvim
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	},
-	checker = { enabled = true },
+    spec = {
+        { import = "plugins" },
+        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    },
+    checker = { enabled = true },
 })
 
 -- Require configs
@@ -13,48 +13,48 @@ require("config.mason-lsp") -- Mason/lsp config
 
 -- Telescope configuration
 require('telescope').setup {
-	defaults = {
-		vimgrep_arguments = {
-			'rg',
-			'--color=never',
-			'--no-heading',
-			'--with-filename',
-			'--line-number',
-			'--column',
-			'--smart-case',
-			'--hidden',
-		},
-	},
-	pickers = {
-		find_files = { hidden = true },
-	},
+    defaults = {
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+        },
+    },
+    pickers = {
+        find_files = { hidden = true },
+    },
 }
 
 -- nvim-treesitter configuration
 require("nvim-treesitter.configs").setup({
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-	},
-	ensure_installed = {
-		"json", "javascript", "typescript", "tsx", "yaml", "html", "css", "markdown",
-		"markdown_inline", "bash", "lua", "vim", "dockerfile", "c", "c_sharp", "cpp",
-		"rust", "go", "hyprlang", "zig"
-	},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+    },
+    ensure_installed = {
+        "json", "javascript", "typescript", "tsx", "yaml", "html", "css", "markdown",
+        "markdown_inline", "bash", "lua", "vim", "dockerfile", "c", "c_sharp", "cpp",
+        "rust", "go", "hyprlang", "zig"
+    },
 })
 
 -- nvim-tree configuration
 require("nvim-tree").setup {
-	open_on_tab = false,
-	update_focused_file = { enable = true, update_cwd = true },
+    open_on_tab = false,
+    update_focused_file = { enable = true, update_cwd = true },
 }
 
 -- Lualine status bar
 require("lualine").setup {
-	options = { theme = 'catppuccin' },
+    options = { theme = 'catppuccin' },
 }
 
 -- Snippets
@@ -63,33 +63,33 @@ require('luasnip.loaders.from_vscode').lazy_load()
 -- Completion (nvim-cmp)
 local cmp = require('cmp')
 cmp.setup({
-	snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
-	mapping = cmp.mapping.preset.insert({
-		['<Down>'] = cmp.mapping.select_next_item(),
-		['<Tab>'] = cmp.mapping.select_next_item(),
-		['<Up>'] = cmp.mapping.select_prev_item(),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm({ select = false }),
-	}),
-	sources = {
-		{ name = 'luasnip' },
-		--{ name = 'nvim_lsp' },
-		{ name = 'buffer' },
-		{ name = 'path' },
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	formatting = {
-		format = require('lspkind').format,
-	},
+    snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
+    mapping = cmp.mapping.preset.insert({
+        ['<Down>'] = cmp.mapping.select_next_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<Up>'] = cmp.mapping.select_prev_item(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    }),
+    sources = {
+        { name = 'luasnip' },
+        { name = 'nvim_lsp', max_item_count = 3 },
+        { name = 'buffer' },
+        { name = 'path' },
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+        format = require('lspkind').format,
+    },
 })
 
 -- nvim-cmp autopairs (bracket/parenthesis completion)
 local npairs = require('nvim-autopairs')
 npairs.setup({
-	check_ts = true,
+    check_ts = true,
 })
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
@@ -100,30 +100,30 @@ require("lsp_lines").setup()
 
 -- Conform formatter setup
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "isort", "black" },
-		rust = { "rustfmt", lsp_format = "fallback" },
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-		typescript = { "prettierd", "prettier", stop_after_first = true },
-		go = { "gofmt" }, -- Or "gofumpt"
-		sh = { "shfmt" },
-		bash = { "shfmt" },
-		html = { "prettier" },
-		zig = { "zigfmt" }, -- Ensure 'zig' is in your PATH
-		c = { "clang-format" },
-		cpp = { "clang-format" },
-		objc = { "clang-format" },
-		objcpp = { "clang-format" },
-	},
+    formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        go = { "gofmt" }, -- Or "gofumpt"
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+        html = { "prettier" },
+        zig = { "zigfmt" }, -- Ensure 'zig' is in your PATH
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        objc = { "clang-format" },
+        objcpp = { "clang-format" },
+    },
 })
 
 -- nvim-lint setup
 local lint = require("lint")
 lint.linters_by_ft = {
-	sh = { "shellcheck" },
-	bashrc = { "shellcheck" },
-	env = { "shellcheck" },
+    sh = { "shellcheck" },
+    bashrc = { "shellcheck" },
+    env = { "shellcheck" },
 }
 
 -- ( DAP AND LINTERS NOT CONFIGURED YET, REMEMBER TO DO SO )
