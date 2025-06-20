3c78cd23a6e35aa47a524c8177540e499bfe3de7
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-06-20 17:15:50 +0800

    Update mason-lsp.lua

--- nvim/.config/nvim/lua/config/mason-lsp.lua
+++ nvim/.config/nvim/lua/config/mason-lsp.lua
@@ -1,26 +1,35 @@
 -- Mason setup to manage both LSPs and formatters
 require("mason").setup({})
 require("mason-tool-installer").setup({
-	ensure_installed = {
-		-- LSP Servers
-		"basedpyright",
-		"rust_analyzer",
-		"gopls",
-		"bashls",
-		"html",
-		"zls",
-		"ts_ls",
-		"clangd",
-		"lua_ls",
-		-- Formatters
-		"stylua",
-		"black",
-		"isort",
-		"prettier",
-		"shfmt",
-		"clang-format",
-		"crlfmt",
-	},
+    ensure_installed = {
+        -- LSP Servers
+        "basedpyright",
+        "rust_analyzer",
+        "gopls",
+        "bashls",
+        "html",
+        "zls",
+        "ts_ls",
+        "clangd",
+        "lua_ls",
+        -- Formatters
+        "stylua",
+        "black",
+        "isort",
+        "prettier",
+        "shfmt",
+        "clang-format",
+        "crlfmt",
+        -- Linters
+        "pylint",        -- python
+        "golangci-lint", -- GO
+        "shellcheck",    -- bash
+        "htmlhint",      -- html
+        "eslint_d",      -- ts_ls
+        "cpplint",       -- C/C++
+        "luacheck",      -- lua
+    },
+    auto_update = true,
 })
 
 -- LSP Configuration
@@ -28,24 +37,24 @@ local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("mason-lspconfig").setup({
-	handlers = {
-		function(server_name)
-			if server_name == "lua_ls" then
-				lspconfig[server_name].setup {
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							diagnostics = { globals = { 'vim' } },
-							workspace = { checkThirdParty = false },
-							telemetry = { enable = false },
-							completion = { callSnippet = "Replace" },
-							hint = { enable = true },
-						},
-					},
-				}
-			else
-				lspconfig[server_name].setup { capabilities = capabilities }
-			end
-		end,
-	},
+    handlers = {
+        function(server_name)
+            if server_name == "lua_ls" then
+                lspconfig[server_name].setup {
+                    capabilities = capabilities,
+                    settings = {
+                        Lua = {
+                            diagnostics = { globals = { 'vim' } },
+                            workspace = { checkThirdParty = false },
+                            telemetry = { enable = false },
+                            completion = { callSnippet = "Replace" },
+                            hint = { enable = true },
+                        },
+                    },
+                }
+            else
+                lspconfig[server_name].setup { capabilities = capabilities }
+            end
+        end,
+    },
 })
