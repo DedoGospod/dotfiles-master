fce3ba9ec651c724744681aacfab440c12182028
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-07-23 04:22:51 +0800

    Update lualine.lua

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,5 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = 'VeryLazy', -- This will load it very early, but still lazy
 }
