e632e1595f713e5a226bd638cf5741d515ce9096
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-07-23 03:30:42 +0800

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -59,9 +59,9 @@ vim.filetype.add({
 })
 
 -- Formatting on Save
-vim.api.nvim_create_autocmd("BufWritePre", {
-    group = vim.api.nvim_create_augroup("LspFormat", { clear = true }),
-    callback = function()
-        vim.lsp.buf.format({ async = false }) -- Set async to false for synchronous formatting
-    end,
-})
+--vim.api.nvim_create_autocmd("BufWritePre", {
+--    group = vim.api.nvim_create_augroup("LspFormat", { clear = true }),
+--    callback = function()
+--        vim.lsp.buf.format({ async = false }) -- Set async to false for synchronous formatting
+--    end,
+--})
