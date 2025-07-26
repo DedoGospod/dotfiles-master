a39cd0399d26608fa229b73db5f8376778c069a9
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-07-26 17:49:59 +0800

    Update neotree.lua

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,15 +1,12 @@
 return {
+  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-        { "3rd/image.nvim", opts = {} },
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    opts = {
-        -- fill any relevant options here
-    },
+  }
 }
