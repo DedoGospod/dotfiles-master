9db815a3c111e8079202d145f61352fde6a6b7b4
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-07-28 11:19:55 +0800

    Create completions.lua

---
+++ nvim/.config/nvim/lua/config/completions.lua
@@ -0,0 +1,48 @@
+-- Completion (nvim-cmp)
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<Down>'] = cmp.mapping.select_next_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(), -- This is your current mapping
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<Up>'] = cmp.mapping.select_prev_item(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<BS>'] = cmp.mapping.abort(),
+    }),
+    sources = {
+        { name = 'luasnip',  max_item_count = 3 },
+        { name = 'nvim_lsp', max_item_count = 3 },
+        { name = 'buffer',   max_item_count = 3 },
+        { name = 'path',     max_item_count = 3 },
+        { name = 'cmdline',  max_item_count = 5 },
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+        format = require('lspkind').format,
+    },
+})
+
+-- nvim-cmp autopairs (bracket/parenthesis completion)
+local npairs = require('nvim-autopairs')
+npairs.setup({
+    check_ts = true,
+})
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
