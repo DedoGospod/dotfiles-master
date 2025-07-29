19f6ce067d5ed519fad40c5f9969a0624ebcb76b
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-07-29 10:01:41 +0800

    Update lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -40,6 +40,29 @@ require("nvim-treesitter.configs").setup({
     },
 })
 
+-- Harpoon setup
+local harpoon = require('harpoon')
+harpoon:setup({})
+
+-- basic harpoon telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<leader>ls", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
 
 -- Telescope configuration
 require('telescope').setup {
