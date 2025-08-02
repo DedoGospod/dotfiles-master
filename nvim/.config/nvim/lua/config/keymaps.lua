783d61b9661a77f5e131e72c21b89e41fb175568
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-08-02 15:29:55 +0800

    Update keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -61,6 +61,7 @@ end, { desc = "Format current buffer with conform" })
 -- Harpoon
 local harpoon = require("harpoon")
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<Leader>ls", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
