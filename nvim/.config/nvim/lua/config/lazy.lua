5241020748b89e913b8ff551af390a2e4bc8296e
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-08-02 19:06:11 +0800

    Update lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -79,6 +79,7 @@ harpoon:setup({})
 
 -- Snippets
 require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_snipmate').lazy_load()
 
 -- LSP lines setup
 require("lsp_lines").setup()
