0ea6a44d59496f0ddd48eb8c61b130da5558c7cf
DedoGospod <libsecret>
2025-11-05 16:05:23 +0800

    test4

--- nix/nix-homemanager-install.sh
+++ nix/nix-homemanager-install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# This command fetches and adds the Home Manager signing key
-nix-shell -p nix-prefetch-url --run "nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager"
 
 # Add nix home manager channel
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
 
 # Update the channels to fetch the packages
 nix-channel --update
