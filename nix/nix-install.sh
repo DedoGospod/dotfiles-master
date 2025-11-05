5d7e361d6e57fb46b622a234da400d38b7bb42e5
DedoGospod <libsecret>
2025-11-05 16:12:17 +0800

    test5

---
+++ nix/nix-install.sh
@@ -0,0 +1,14 @@
+sudo pacman -S nix   
+
+sudo systemctl enable --now nix-daemon
+
+sudo gpasswd -a $USER nix-users   
+
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+nix-shell '<home-manager>' -A install
+
+home-manager switch
