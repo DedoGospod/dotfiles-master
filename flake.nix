6d9b50f8e77410a0a543609f475b9b60bb4f4cfe
DedoGospod <dylanlazarov2002@protonmail.com>
2025-11-08 13:57:48 +0800

    flakes

--- flake.nix
+++ flake.nix
@@ -1,15 +1,26 @@
+{
+  description = "Dylan's Home Manager Flake (Standalone)";
+
+inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
 outputs = { self, nixpkgs, home-manager, ... }: {
-  # Define a home-manager configuration for your user "tony"
-  homeConfigurations.tony = home-manager.lib.homeManagerConfiguration {
+  homeConfigurations.dylan = home-manager.lib.homeManagerConfiguration {
     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Or whatever system you target
     modules = [
-      # The home.nix file you were importing
-      ./home-manager/home.nix
+      ./home-manager/.config/home-manager/home.nix
       {
         home.username = "dylan";
-        home.homeDirectory = "/home/dylan"
-        home.stateVersion = "24.05";
+        home.homeDirectory = "/home/dylan";
+        home.stateVersion = "25.05";
       }
     ];
   };
 };
+
+}
